f4eb6e3397c173da81855027becb3f72925cba02
takuyaw-w <takuya.jamtb@outlook.com>
2019-03-24 08:58:20 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '2dbplayer'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
